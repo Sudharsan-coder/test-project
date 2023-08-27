250eaedc2c5e5a76d1ed9b4401518ba1cf655047
YokeshSeeker <yokeshgk2003@gmail.com>
2023-08-27 11:43:43 +0530

    feat: add findByID

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -17,26 +17,167 @@ const createUser = async (userName:string, googleId:string, userImg:string, user
         userImg: userImg,
         userEmail: userEmail,
     }
-    return await Users.create(user)
+    console.log('userCreated');
+    
+    const newUser = await Users.create(user);
+
+    return newUser;
 }
 
-const findUser = async (googleId:string) => {
+const findById = async (googleId:string) => {
     if(!googleId){
         return error({
             status: 400,
             message: 'User Not Found'
         });
     }
-    return await Users.findAll({
+    console.log('userFound');
+
+    const user = await Users.findAll({
         where: {
             googleId: googleId
         }
     })
+
+    return user;
+}
+
+const getAllUsers = async () => {
+    const result = await Users.findAll({
+        include: [{
+            model: table.Ratings,
+            as: 'ratings',
+        },
+        {
+            model: table.Skills,
+            as: 'skills'
+        }]
+    })
+    // console.log(JSON.stringify(result, null, 2));
+    return result;
 }
 
 const userController = {
     createUser,
-    findUser
+    findById,
+    getAllUsers
 }
 
 export default userController;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getAllUsers = async (weekNum:number) => {
+//     const result = await Users.findOrCreate({
+//         include: [{
+//             model: table.Ratings,
+//             as: 'ratings',
+//             where: {weekNum: weekNum}
+//         },
+//         {
+//             model: table.Skills,
+//             as: 'skills'
+//         }
+//     ]
+//     })
+//     return result
+// }
+
+// const getAllUsers = async (weekNum:number) => {
+
+//     const userIds = await table.Ratings.findAll({
+//         attributes: ['userId'],
+//     })
+
+//     const userIdsArray = userIds.map((userId:any) => userId.userId)
+
+//     // get all userIds
+//     const alluserid = await table.Users.findAll({});
+//     // findOrCreate ratings for all userIds where weekNum = weekNum
+//     const allUserRatings = await table.Ratings.findOrCreate({
+//         where: { 
+//             weekNum: 35,
+//             userId: alluserid
+//         },
+//         defaults: {
+//             weekNum: 35,
+//             teamPlay: 0,
+//             attitude: 0,
+//             technicalExpertise: 0,
+//             codingSkills: 0,
+//             overAllScore: 0,
+//         }
+//     });
+
+//     // const result = await Users.findAll({
+//     //     include: [{
+//     //         model: table.Ratings,
+//     //         as: 'ratings',
+//     //         where: {weekNum: 35}
+//     //     },
+//     //     {
+//     //         model: table.Skills,
+//     //         as: 'skills'
+//     //     }],
+//     //     where: {id: userIdsArray}
+//     // })
+
+//     console.log(JSON.stringify(allUserRatings, null, 2));
+    
+//     return allUserRatings;
+// }
