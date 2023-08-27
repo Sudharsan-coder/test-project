95f3b891e10a7a8211a3dde76d0bf25a3e0edfa1
YokeshSeeker <yokeshgk2003@gmail.com>
2023-08-27 11:43:23 +0530

    fix: remove phase

--- server/controllers/skills.controller.ts
+++ server/controllers/skills.controller.ts
@@ -2,10 +2,9 @@ const table = require('../models');
 
 const Skills = table.Skills;
 
-const createSkill = async (skills:Array<string>,phase:string,userId:number|null) => {
+const createSkill = async (skills:Array<string>,userId:number|null) => {
     const skill = {
         skills: skills,
-        phase: phase,
         userId: userId
     }
     return await Skills.create(skill)
